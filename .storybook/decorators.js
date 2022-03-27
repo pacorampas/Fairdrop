fee46dd45b4e1dd4120214da7a0bb3d977ca5d66
Paco Rampas <pacorampas@gmail.com>
2022-03-27 12:05:02 +0200

    global css into stortbook

--- .storybook/decorators.js
+++ .storybook/decorators.js
@@ -1,6 +1,7 @@
 import { GlobalCSS } from '../src/theme/GlobalCSS'
 import { theme } from '../src/theme/theme'
 import { ThemeProvider } from 'styled-components'
+import '../src/index.css'
 
 const withTheme = (Story) => (
   <ThemeProvider theme={theme}>
